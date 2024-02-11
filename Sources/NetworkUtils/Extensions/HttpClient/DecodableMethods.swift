f2bd851d59c7df9b22d4f7e7378d8cf0d6fa66ad
Jorge Luis Toledo <85928995+toledo85@users.noreply.github.com>
2024-02-11 10:38:06 -0500

    Update HttpClient decodable extension

--- Sources/NetworkUtils/Extensions/HttpClient/DecodableMethods.swift
+++ Sources/NetworkUtils/Extensions/HttpClient/DecodableMethods.swift
@@ -9,10 +9,13 @@ import Foundation
 
 extension HttpClient {
     @available(iOS 13.0.0, macOS 10.15.0, *)
-    public func getFromJson<T: Decodable>(to request: URLRequest) async throws -> T {
+    public func getFromJson<T: Decodable>(
+        to request: URLRequest,
+        decoder: JSONDecoder = JSONDecoder()
+    ) async throws -> T {
         let (data, _) = try await perform(from: request)
 
-        guard let object = try? JSONDecoder().decode(T.self, from: data) else {
+        guard let object = try? decoder.decode(T.self, from: data) else {
             throw HttpError.errorDecodingData
         }
         
@@ -20,40 +23,59 @@ extension HttpClient {
     }
     
     @available(iOS 13.0.0, macOS 10.15.0, *)
-    public func getFromJson<T: Decodable>(to url: String) async throws -> T {
+    public func getFromJson<T: Decodable>(
+        to url: String,
+        decoder: JSONDecoder = JSONDecoder()
+    ) async throws -> T {
         let request = try URLRequest.createRequest(from: url)
-        return try await getFromJson(to: request)
+        return try await getFromJson(to: request, decoder: decoder)
     }
     
     @available(iOS 13.0.0, macOS 10.15.0, *)
-    public func postAsJson(to request: URLRequest, object: some Encodable) async throws -> (Data, HTTPURLResponse) {
+    public func postAsJson(
+        to request: URLRequest,
+        object: some Encodable,
+        encoder: JSONEncoder = JSONEncoder()
+    ) async throws -> (Data, HTTPURLResponse) {
         var request = request
         request.set(method: .post)
         request.set(header: .contentType, value: MimeType.json.rawValue)
-        request.httpBody = try JSONEncoder().encode(object)
+        request.httpBody = try encoder.encode(object)
                 
         return try await perform(from: request)
     }
     
     @available(iOS 13.0.0, macOS 10.15.0, *)
-    public func postAsJson(to url: String, object: (some Encodable)? = nil) async throws ->  (Data, HTTPURLResponse) {
+    public func postAsJson(
+        to url: String,
+        object: (some Encodable)? = nil,
+        encoder: JSONEncoder = JSONEncoder()
+    ) async throws -> (Data, HTTPURLResponse) {
         let request = try URLRequest.createRequest(from: url)
-        return try await postAsJson(to: request, object: object)
+        return try await postAsJson(to: request, object: object, encoder: encoder)
     }
     
     @available(iOS 13.0.0, macOS 10.15.0, *)
-    public func put(to request: URLRequest, object: some Encodable) async throws -> (Data, HTTPURLResponse) {
+    public func put(
+        to request: URLRequest,
+        object: some Encodable,
+        encoder: JSONEncoder = JSONEncoder()
+    ) async throws -> (Data, HTTPURLResponse) {
         var request = request
         request.set(method: .put)
         request.set(header: .contentType, value: MimeType.json.rawValue)
-        request.httpBody = try JSONEncoder().encode(object)
+        request.httpBody = try encoder.encode(object)
         
         return try await perform(from: request)
     }
     
     @available(iOS 13.0.0, macOS 10.15.0, *)
-    public func put(to url: String, object: some Encodable) async throws ->  (Data, HTTPURLResponse) {
+    public func put(
+        to url: String,
+        object: some Encodable,
+        encoder: JSONEncoder = JSONEncoder()
+    ) async throws -> (Data, HTTPURLResponse) {
         let request = try URLRequest.createRequest(from: url)
-        return try await put(to: request, object: object)
+        return try await put(to: request, object: object, encoder: encoder)
     }
 }
